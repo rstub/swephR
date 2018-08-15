f9aca0070ac671b9ff0d0c727e3c72d978c31bfb
Victor Reijs <web.victor.reijs@gmail.com>
2018-08-15 02:23:51 +0100

    Update some-test-lines.R

--- tests/some-test-lines.R
+++ tests/some-test-lines.R
@@ -3,7 +3,7 @@ library(swephR)
 
 swe_set_ephe_path("C:\\ARCHAEOCOSMO\\ephe")
 swe_set_topo(0,50,10)
-test_that("existing star", {
+test_that("Sxisting star position", {
   result <- swe_fixstar("sirius",1234567,34818)
   expect_true(is.list(result))
   expect_equal(result$return, 34818)
@@ -13,7 +13,7 @@ test_that("existing star", {
 
 
 
-test_that("existing star", {
+test_that("Azimuth and altitude postions", {
   result <- swe_azalt(1234567,1,c(0,50,10),15,1013.25,c(186,22))
   expect_equal(result$xaz, c(114.636188,   8.210881,   8.209274),tolerance = .0000001)
 })
