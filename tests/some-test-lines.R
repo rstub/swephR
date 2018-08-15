638da3fb911cf20e91fe5327d5d9af6cb0a5b4e2
vreijs <web.victor.reijs@gmail.com>
2018-08-15 01:59:07 +0100

    chnage the test stataments

--- tests/some-test-lines.R
+++ tests/some-test-lines.R
@@ -3,21 +3,18 @@ library(swephR)
 
 swe_set_ephe_path("C:\\ARCHAEOCOSMO\\ephe")
 swe_set_topo(0,50,10)
-swe_fixstar("sirius",1234567,34818)
-# $`return`
-# [1] 34818
-# 
-# $star
-# [1] "Sirius,alCMa"
-# 
-# $xx
-# [1]  6.465315e+01 -1.780315e+01  5.518192e+05 -3.482530e-04  1.170200e-05 -3.109594e-03
-# 
-# $serr
-# [1] "SwissEph file 'seplm18.se1' not found in PATH 'C:\\ARCHAEOCOSMO\\ephe\\' \nusing Moshier eph.; "
+test_that("Existing star position", {
+  result <- swe_fixstar("sirius",1234567,34818)
+  expect_true(is.list(result))
+  expect_equal(result$return, 34818)
+  expect_equal(result$xx, c(6.465315e+01, -1.780315e+01,  5.518192e+05, -3.482530e-04,  1.170200e-05, -3.109594e-03),tolerance = .00001)
+  expect_equal(result$serr, "SwissEph file 'seplm18.se1' not found in PATH 'C:\\ARCHAEOCOSMO\\ephe\\' \nusing Moshier eph.; ")
+})
 
 
 
-swe_azalt(1234567,1,c(0,50,10),15,1013.25,c(186,22))
-# $`xaz`
-# [1] 114.636188   8.210881   8.209274
+
+test_that("Azimuth and altitude calculations", {
+  result <- swe_azalt(1234567,1,c(0,50,10),15,1013.25,c(186,22))
+  expect_equal(result$xaz, c(114.636188,   8.210881,   8.209274),tolerance = .0000001)
+})
