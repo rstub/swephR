1cc51245f0a0d8456742719d6d1e99476ad61169
Ralf Stubner <ralf.stubner@gmail.com>
2018-09-08 01:17:12 +0200

    Unset SE_EPHE_PATH before loading swephR

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,8 @@
-library(testthat)
-library(swephR)
-
 ## inhibit overloading of ephemeris provided by
 ## swephR and swephRdata
 Sys.setenv(SE_EPHE_PATH = "")
+
+library(testthat)
+library(swephR)
+
 test_check("swephR")
