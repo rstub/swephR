c67f3352cf8288bdd214ea9bb21953f27303f573
Ralf Stubner <ralf.stubner@gmail.com>
2018-09-13 00:58:37 +0200

    Add upstream link in DESCRIPTION

--- R/swephR-package.R
+++ R/swephR-package.R
@@ -1,8 +1,3 @@
-##' R interface to the Swiss Ephemeris
-##'
-##' The swephR package provides an R interface for the
-##' \href{http://www.astro.com/swisseph/swephinfo_e.htm}{\code{Swiss Ephemeris}}.
-##'
 ##' @useDynLib swephR, .registration = TRUE
 ##' @importFrom Rcpp evalCpp
 "_PACKAGE"
