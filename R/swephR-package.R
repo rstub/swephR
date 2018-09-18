62f580ece4ab7b577002ea978e52af3606faa35a
Ralf Stubner <ralf.stubner@gmail.com>
2018-09-18 23:10:23 +0200

    Add links to JPL and SE docu

--- R/swephR-package.R
+++ R/swephR-package.R
@@ -1,3 +1,8 @@
+##' @details The current set of functions is closely modled after the original
+##'   C API of the Swiss Ephemeris. It is therefore useful to use the general
+##'   documentation available at \url{http://www.astro.com/swisseph/swisseph.htm}
+##'   as well as the programmer's documentation available at
+##'   \url{http://www.astro.com/swisseph/swephprg.htm}.
 ##' @useDynLib swephR, .registration = TRUE
 ##' @importFrom Rcpp evalCpp
 "_PACKAGE"
