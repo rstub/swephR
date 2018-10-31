4b9b270308d318ab47c8035e2352fd6f5931a3ab
Ralf Stubner <ralf.stubner@gmail.com>
2018-11-01 00:14:52 +0100

    Include necessary array header

--- src/Section3.cpp
+++ src/Section3.cpp
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with swephR.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <array>
 #include <Rcpp.h>
 #include <swephexp.h>
 
