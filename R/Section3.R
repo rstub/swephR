b74776409cabdb99da8a088e5b192e69f27c7080
vreijs <web.victor.reijs@gmail.com>
2018-11-07 22:10:38 +0000

    Get functions to behave when the input is a vector in itself. Perhaps it needs to be a list of parameter values (and a parameter value can be a vector).

--- R/Section3.R
+++ R/Section3.R
@@ -1,32 +1,73 @@
 M_get_planet_name <- function(ipl) {
-  
   functionvector <- data.frame(ipl)
   print(functionvector)
   ResultVector <- c(0)
   for (i in 1:nrow(functionvector))
   {
-    ResultVector[i] = swe_get_planet_name(functionvector$ipl[i])
+    ResultVector[i] <- swe_get_planet_name(functionvector$ipl[i])
   }
   return(ResultVector)
 }
 
-M_sol_eclipse_when_loc <- function(jd_start,ephe_flag, long,lat,height, backward) {
-  
-  functionvector <- data.frame(jd_start,ephe_flag, long,lat,height, backward)
-  print(functionvector)
-  ResultVector <- c(0)
-  for (i in 1:nrow(functionvector))
-  {
-    geopos<-c(functionvector$long[i],functionvector$lat[i],functionvector$height[i])
-    ResultA <- swe_sol_eclipse_when_loc(functionvector$jd_start[i],
-                                               functionvector$ephe_flag[i],
-                                               geopos,
-                                               functionvector$backward[i])
-    print(ResultA)
-   a<-0
-                                               
-                                              
+M_sol_eclipse_when_loc <-
+  function(jd_start,
+           ephe_flag,
+           long,
+           lat,
+           height,
+           backward) {
+    functionvector <-
+      data.frame(jd_start, ephe_flag, long, lat, height, backward)
+    print(functionvector)
+    ResultVector <- NULL
+    for (i in 1:nrow(functionvector))
+    {
+      geopos <-
+        c(functionvector$long[i],
+          functionvector$lat[i],
+          functionvector$height[i])
+      ResultA <- swe_sol_eclipse_when_loc(
+        functionvector$jd_start[i],
+        functionvector$ephe_flag[i],
+        geopos,
+        functionvector$backward[i]
+      )
+     # print(ResultA)
+      if (i == 1) {
+          ResultVector[[i]]<- ResultA
+        } else {
+          ResultVector[[i]] <- ResultA
+        }
+    }
+    return(ResultVector)
+    
   }
-  return(ResultVector)
-  
-}
+
+#trialing how to get the function behaving with variables that are itself alreday vectors
+P_sol_eclipse_when_loc <-
+  function(jd_start,
+           ephe_flag,
+           geopos,
+           backward) {
+    functionvector <-
+      data.frame(jd_start, ephe_flag, geopos, backward)
+    print(functionvector)
+    ResultVector <- NULL
+    for (i in 1:nrow(functionvector))
+    {
+      ResultA <- swe_sol_eclipse_when_loc(
+        functionvector$jd_start[i],
+        functionvector$ephe_flag[i],
+        functionvector$geopos[i],
+        functionvector$backward[i]
+      )
+      # print(ResultA)
+      if (i == 1) {
+        ResultVector[[i]]<- ResultA
+      } else {
+        ResultVector[[i]] <- ResultA
+      }
+    }
+    return(ResultVector)
+    
+  }
