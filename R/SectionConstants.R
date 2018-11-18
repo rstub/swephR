b42f76ceab0ce3f3b621af7fe1ffb7dc9b3084eb
vreijs <web.victor.reijs@gmail.com>
2018-11-18 10:39:11 +0000

    removed a few constants, still don;t know how to load SE.rda (as the command is OS depending).

--- R/SectionConstants.R
+++ R/SectionConstants.R
@@ -415,8 +415,5 @@ SEConstants <- function() {
         SE$HELFLAG_AVKIND_PTO   + SE$HELFLAG_AVKIND_MIN7   +
         SE$HELFLAG_AVKIND_MIN9
     )
-  #some variables not starting with SE. Perhaps they could be removed
-  SE$TJD_INVALID		<- 	99999999.0
-  SE$SIMULATE_VICTORVB        <-       1
   return(SE)
 }
