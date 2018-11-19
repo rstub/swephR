aa979a8cf6e3053589a0415a25e8eface1154171
vreijs <web.victor.reijs@gmail.com>
2018-11-19 19:33:08 +0000

    I think I have correct now the vectorised input (for multi-situation) and then as a result a list. I also kep tthe swe_* function close to the SE definitions (but added R-functions around them to vectorise them).

--- R/Section3.R
+++ R/Section3.R
@@ -1,26 +1,28 @@
-M_get_planet_name <- function(ipl) {
+get_planet_name <- function(ipl) {
   functionvector <- data.frame(ipl)
   print(functionvector)
-  ResultVector <- c(0)
-  for (i in 1:nrow(functionvector))
+  listsize<-nrow(functionvector)
+  ResultVector <- vector("list",listsize)
+  for (i in 1:listsize)
   {
     ResultVector[[i]] <- swe_get_planet_name(functionvector$ipl[i])
   }
   return(ResultVector)
 }
 
-M_sol_eclipse_when_loc <-
+sol_eclipse_when_loc <-
   function(jd_start,
-           ephe_flag,
+           ephe_flag = 4, # default Moshier epheemris
            long,
            lat,
-           height,
-           backward) {
+           height = 0,
+           backward = FALSE) {
     functionvector <-
       data.frame(jd_start, ephe_flag, long, lat, height, backward)
     print(functionvector)
-    ResultVector <- vector("list",nrow(functionvector))
-    for (i in 1:nrow(functionvector))
+    listsize<-nrow(functionvector)
+    ResultVector <- vector("list",listsize)
+    for (i in 1:listsize)
     {
       geopos <-
         c(functionvector$long[i],
@@ -30,29 +32,6 @@ M_sol_eclipse_when_loc <-
         functionvector$jd_start[i],
         functionvector$ephe_flag[i],
         geopos,
-        functionvector$backward[i]
-      )
-    }
-    return(ResultVector)
-    
-  }
-
-#trialing how to get the function behaving with variables that are itself alreday vectors
-P_sol_eclipse_when_loc <-
-  function(jd_start,
-           ephe_flag,
-           geopos,
-           backward) {
-    functionvector <-
-      data.frame(jd_start, ephe_flag, geopos, backward)
-    print(functionvector)
-    ResultVector <- NULL
-    for (i in 1:nrow(functionvector))
-    {
-      ResultVector[[i]] <- swe_sol_eclipse_when_loc(
-        functionvector$jd_start[i],
-        functionvector$ephe_flag[i],
-        functionvector$geopos[i],
         functionvector$backward[i]
       )
     }
