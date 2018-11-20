24d09960a7b2e49ffd6ebe3aeab4ebc02825eb57
vreijs <web.victor.reijs@gmail.com>
2018-11-20 23:09:46 +0000

    documentatin for the SE-constants dataset

---
+++ R/SEdata.R
@@ -0,0 +1,14 @@
+#' Constants used in swephR
+#'
+#'
+#' \itemize{
+#'   \item name of variable
+#'   \item value of the variable
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name SE
+#' @usage data(SE)
+#' @format A data frame with 217 rows and 2 variables
+NULL
