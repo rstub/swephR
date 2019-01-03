4f99ad1cefba4f2f5b8dfd3986d9af5705e65441
Ralf Stubner <ralf.stubner@gmail.com>
2019-01-03 18:46:04 +0100

    Use 'mustWork = TRUE' when looking for ephemeris files (fixes #24)

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
     if(requireNamespace("swephRdata", quietly = TRUE)) {
-        swe_set_ephe_path(system.file("ephemeris", package = "swephRdata"))
+        swe_set_ephe_path(system.file("ephemeris", package = "swephRdata", mustWork = TRUE))
     } else {
-        swe_set_ephe_path(system.file("ephemeris", package = "swephR"))
+        swe_set_ephe_path(system.file("ephemeris", package = "swephR", mustWork = TRUE))
     }
 }
