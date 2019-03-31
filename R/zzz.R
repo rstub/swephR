00403c0e7cfc1c9a0591a501827a4a33e73b5923
Ralf Stubner <ralf.stubner@gmail.com>
2019-03-31 12:44:00 +0200

    Clean-up in case package is unloaded

--- R/zzz.R
+++ R/zzz.R
@@ -5,3 +5,8 @@
         swe_set_ephe_path(system.file("ephemeris", package = "swephR", mustWork = TRUE))
     }
 }
+
+.onUnload <- function (libpath) {
+    swe_close()
+    library.dynam.unload("swephR", libpath)
+}
